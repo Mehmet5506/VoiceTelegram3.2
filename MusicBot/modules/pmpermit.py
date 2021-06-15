42f34244fdd3fee79b87ba33a9561ad7371b3403
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-15 20:46:05 +0530

    Update pmpermit.py

--- MusicBot/modules/pmpermit.py
+++ MusicBot/modules/pmpermit.py
@@ -1,13 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from MusicBot.config import MT_PM_MESSAGE_TEXT
+
 from MusicBot.services.callsmusic.callsmusic import client as USER
 
 
-@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmPermit(client: USER, message: Message):
-    await USER.send_message(
-        message.chat.id,
-        " MT_PM_MESSAGE_TEXT ",
-    )
-    return
