f1a4297610fe1dab07e6afa324162cc073e036bf
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-07-15 23:48:09 +0300

    Update ytsearch.py

--- VCsMusicBot/modules/ytsearch.py
+++ VCsMusicBot/modules/ytsearch.py
@@ -22,7 +22,7 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/aramanın bağımsız değişkene ihtiyacı var!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Lütfen bekleyiniz....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
