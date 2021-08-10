72548f0a0b1529acdefcb697497be551e8978ce8
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-08-10 17:07:55 +0300

    Update song.py

--- VCsMusicBot/modules/song.py
+++ VCsMusicBot/modules/song.py
@@ -59,7 +59,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 @Mp3dinleme_Bot tarafından yüklendi**"
+        rep = "**🎵 La Casa de music tarafından yüklendi**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
